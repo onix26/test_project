1c65cd0dbf55853d1df726254718ca4c20197b33
onix26 <onix2691@yandex.ru>
2021-10-13 11:16:03 +0300

    configuration for Heroku

--- test_project/settings.py
+++ test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*o16=^9=gc_^91e0(g2xb)=8^i+l(27&u-l&%k1r*yy60jzq5$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
